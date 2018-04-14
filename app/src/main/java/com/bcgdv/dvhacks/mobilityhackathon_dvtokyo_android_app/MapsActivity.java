c722042053dbbe38c67048677b4f5fcb7ce2ad7a
Rajkumar Waghmare <rajkumar.waghmare@bcgdv.com>
2018-04-14 12:07:19 -0700

    Added FusedLocationProvideClient

--- app/src/main/java/com/bcgdv/dvhacks/mobilityhackathon_dvtokyo_android_app/MapsActivity.java
+++ app/src/main/java/com/bcgdv/dvhacks/mobilityhackathon_dvtokyo_android_app/MapsActivity.java
@@ -15,25 +15,29 @@ import android.test.mock.MockPackageManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
     private LocationManager mLocationManager;
-    private String mBestProvider;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    private final String TAG = getClass().getSimpleName();
 
     private static final String MSG_GPS_ERROR = "GPS Error!";
-    private static final String MSG_NO_LOCATION_PERM_ERROR = "Please enable location permission";
     private static final String MSG_LOCATION_ERROR = "Please get permissions!";
+    private static final String MSG_PERM_GRANTED = "Permission granted";
     private static final int REQUEST_CODE_LOCATION_PERMISSION = 101;
-    private static final int REQUEST_CODE_LOCATION_SETTINGS = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,30 +47,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        mBestProvider = String.valueOf(mLocationManager.getBestProvider(new Criteria(), true));
 
         /**
          * Get the location premissions if not yet
          */
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_CODE_LOCATION_PERMISSION);
         }
 
 
-        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(myIntent);
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mMapFragment.getMapAsync(this);
+        /**
+         * Make sure GPS is enabled, else direct user to setting screen.
+         */
+        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(myIntent);
+        } else {
+            mMapFragment.getMapAsync(this);
+        }
+
     }
 
     /**
@@ -82,10 +91,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_CODE_LOCATION_PERMISSION);
             return;
         }
@@ -97,59 +105,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setBuildingsEnabled(true);
         mMap.setIndoorEnabled(true);
 
-        /**
-         * Make sure GPS is enabled, else direct user to setting screen.
-         */
-        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Toast.makeText(this, MSG_NO_LOCATION_PERM_ERROR, Toast.LENGTH_SHORT);
-            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivityForResult(myIntent, REQUEST_CODE_LOCATION_SETTINGS);
-            return;
-        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location currentLocation) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (currentLocation != null) {
+                            // Logic to handle location object
+                            Log.d(TAG, "GPS is on");
+                            final double currentLatitude = currentLocation.getLatitude();
+                            final double currentLongitude = currentLocation.getLongitude();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude, currentLongitude), 15));
+                            /**
+                             *he desired zoom level, in the range of 2.0 to 21.0. Values below this range are set to 2.0, and values above it are set to 21.0.
+                             * Increase the value to zoom in. Not all areas have tiles at the largest zoom levels.
+                             */
+                            mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
+                        } else {
+                            Log.e(TAG, "GPS is off");
+                            Toast.makeText(MapsActivity.this, MSG_GPS_ERROR, Toast.LENGTH_LONG);
+                        }
+                    }
 
-        Location currentLocation = mLocationManager.getLastKnownLocation(mBestProvider);
-        if (currentLocation != null) {
-            Log.d("TAG", "GPS is on");
-            final double currentLatitude = currentLocation.getLatitude();
-            final double currentLongitude = currentLocation.getLongitude();
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude, currentLongitude), 15));
-            /**
-             *he desired zoom level, in the range of 2.0 to 21.0. Values below this range are set to 2.0, and values above it are set to 21.0.
-             * Increase the value to zoom in. Not all areas have tiles at the largest zoom levels.
-             */
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
-        } else {
-            Log.e("TAG", "GPS is off");
-            Toast.makeText(this, MSG_GPS_ERROR, Toast.LENGTH_SHORT);
-        }
+                });
     }
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.d("Req Code", "" + requestCode);
+        Log.d(TAG, "Req Code " + requestCode);
         if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
             if (grantResults.length == 1 &&
                     grantResults[0] == MockPackageManager.PERMISSION_GRANTED) {
                 // Success Stuff here
-                mMapFragment.getMapAsync(this);
+                Log.d(TAG, MSG_PERM_GRANTED);
             } else {
                 Toast.makeText(this, MSG_LOCATION_ERROR, Toast.LENGTH_SHORT);
             }
         }
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_LOCATION_SETTINGS) {
-            // Make sure the request was successful
-            if (resultCode != RESULT_OK) {
-                finish();
-            }
-        }
-    }
-
-
 }
